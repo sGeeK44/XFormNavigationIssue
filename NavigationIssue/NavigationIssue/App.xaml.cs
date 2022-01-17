304d901a2a0eeec19640d3fd7a94070bc90be8df
sGeeK <f.perin@zero-gachis.com>
2022-01-17 09:11:37 +0100

    :tada: Add issue project

---
+++ NavigationIssue/NavigationIssue/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigationIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnStart()
+        {
+            await Task.Delay(1000);
+            MainPage = new MainPage();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
